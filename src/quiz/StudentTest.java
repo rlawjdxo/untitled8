1a403fb28b4074e7c1456bd249772e246c1649d6
rlawjdxo <rlawjd0721@naver.com>
2021-12-11 16:46:04 +0900

    Initial commit

---
+++ src/quiz/StudentTest.java
@@ -0,0 +1,25 @@
+package quiz;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class Student {
+    public static void main(String[] args) {
+        Function<Integer, Integer> student = x -> x * x * x / 3;
+        System.out.print(student.apply("국어 평균":new Student()));
+        System.out.print(student.apply("영어 평균":new Student()));
+        System.out.print(student.apply("수학 평균":new Student()));
+
+
+        static List<Student>
+                students = Arrays.asList(new Student[]("지호진", 100, 99, 100);
+                new Student("오석진", 80, 81, 83);,new Student("박재수", 90, 75, 87);,
+                new Student("노경두", 91, 100, 90);,new Student("박춘규", 89, 87, 71)
+               );
+
+        }
+
+}
+
+
